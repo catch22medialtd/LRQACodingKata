c57e493f89061630faee834bec0dfe6e866a3630
Gul Faraz <gul.faraz@live.co.uk>
2026-03-01 16:17:38 +0000

    Feature: Added interface for repository

---
+++ code/src/LRQACodingKata.Core/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace LRQACodingKata.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+
+        Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+        Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
